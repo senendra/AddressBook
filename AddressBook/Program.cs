050daaa8f416b10f1f6f1290e9d1daeb8911883d
Senendra <senendra.007@gmail.com>
2020-10-31 12:52:19 +0530

    [Senendra] Add: Adding class to one can read or write in CSV file

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -29,7 +29,7 @@ namespace AddressBook
             do
             {
                 Console.WriteLine($"Working on {bookName} AddressBook\n");
-                Console.WriteLine("Enter your Choice:\n1. Add New Contact \n2. Edit Exitisting Contact \n3. Delete A Contact \n4. View A Contact \n5.View All Contact \n6.Add New AddressBook \n7.Switch AddressBook \n8.Search Contact by city/state \nn9.Count Persons COntact \n10.Sort Entry by name \n11.Print in text formate \n0.Exit");
+                Console.WriteLine("Enter your Choice:\n1. Add New Contact \n2. Edit Exitisting Contact \n3. Delete A Contact \n4. View A Contact \n5.View All Contact \n6.Add New AddressBook \n7.Switch AddressBook \n8.Search Contact by city/state \nn9.Count Persons COntact \n10.Sort Entry by name \n11.Read or Write in text \n12.Write in CSV file \n0.Exit");
                 choice = Console.ReadLine();
                 switch (choice)
                 {
@@ -149,6 +149,11 @@ namespace AddressBook
                         FileIOOperations fileIO = new FileIOOperations();
                         fileIO.WriteToFile(addressBook.addressBookDictionary);
                         fileIO.ReadFromFile();
+                        break;
+                    case "12":
+                        CSVFileHandler handler = new CSVFileHandler();
+                        handler.WriteToFile(addressBook.addressBookDictionary);
+                        handler.ReadFromFile();
                         break;
                     case "0":
                         Console.WriteLine("Thank You For Using Address Book System.");
